6bebb8ae12f48170b467025e4b6c8f711ce17faa
Alzex <alexzemly@gmail.com>
2023-04-02 22:29:15 +0300

    Added numpy with buster

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+import numpy
+from fastapi import APIRouter
 
 router = APIRouter()
 
@@ -6,3 +8,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
+
